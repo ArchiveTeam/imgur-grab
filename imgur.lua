d0a8a31437cd021231202f970afc402e0ccbd9b0
Florian Bach <Leseratte10@users.noreply.github.com>
2023-05-14 21:53:34 +0200

    Update imgur.lua
    Limit download attempts to 3

--- imgur.lua
+++ imgur.lua
@@ -566,7 +566,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     io.stdout:write("Server returned bad response. Sleeping.\n")
     io.stdout:flush()
     tries = tries + 1
-    if tries > 8 or status_code == 404 then
+    if tries > 2 or status_code == 404 then
       tries = 0
       abort_item()
       return wget.actions.EXIT
