ea58d80fa1a6cf5e97f6877ab085aa4f15415ee1
arkiver <arkiver@protonmail.com>
2023-05-15 05:56:34 +0200

    Version 20230515.02. Take out old hot fix.

--- pipeline.py
+++ pipeline.py
@@ -60,7 +60,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20230515.01'
+VERSION = '20230515.02'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 TRACKER_ID = 'imgur'
 TRACKER_HOST = 'legacy-api.arpa.li'
