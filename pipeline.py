866ed54e255ed58d7c8b384faf2bedbe6156ecde
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2023-05-16 07:57:36 +0000

    Version 20230516.01. Multi item size 30.

--- pipeline.py
+++ pipeline.py
@@ -60,11 +60,11 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20230515.07'
+VERSION = '20230516.01'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 TRACKER_ID = 'imgur'
 TRACKER_HOST = 'legacy-api.arpa.li'
-MULTI_ITEM_SIZE = 15
+MULTI_ITEM_SIZE = 30
 
 
 ###########################################################################
